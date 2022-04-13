b33d19327ca49d546ba98ed23c9bce6aba3686d8
weiyinfu.weiyinfu <weiyinfu.weiyinfu@bytedance.com>
2022-04-13 18:35:41 +0800

    ignore https；optimize mac behavior

--- src/main/java/wyf/easycrawl/handler/HttpClientRequestHandler.java
+++ src/main/java/wyf/easycrawl/handler/HttpClientRequestHandler.java
@@ -8,7 +8,10 @@ import org.apache.http.config.RegistryBuilder;
 import org.apache.http.config.SocketConfig;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
 import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.AllowAllHostnameVerifier;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustAllStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
@@ -31,55 +34,43 @@ import java.util.concurrent.TimeUnit;
  * 使用apache httpclient的http客户端
  */
 public class HttpClientRequestHandler implements RequestHandler {
-Logger logger = LoggerFactory.getLogger(HttpClientRequestHandler.class);
-CloseableHttpClient client = null;
+    Logger logger = LoggerFactory.getLogger(HttpClientRequestHandler.class);
+    CloseableHttpClient client = null;
 
 
-public HttpClientRequestHandler(boolean ignoreSslError) {
-    logger.info("creating client");
-    var builder = HttpClientBuilder.create()
-            .setConnectionTimeToLive(100, TimeUnit.SECONDS)
-            .disableAutomaticRetries()
-            .setDefaultHeaders(Collections.singletonList(
-                    new BasicHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36")
-            ))
-            .setDefaultConnectionConfig(ConnectionConfig.custom()
-                    .setBufferSize(2048)
-                    .build())
-            .setDefaultSocketConfig(SocketConfig.custom()
-                    .build())
-            .setDefaultRequestConfig(RequestConfig.custom()
-                    .setMaxRedirects(3)
-                    .setCircularRedirectsAllowed(false)
-                    .setConnectionRequestTimeout(1000 * 3)
-                    .setSocketTimeout(1000 * 3)
-                    .setConnectionRequestTimeout(5 * 1000)
-                    .build());
-    if (ignoreSslError) {
+    public HttpClientRequestHandler() {
+        logger.info("creating client");
+        var builder = HttpClientBuilder.create()
+                .setConnectionTimeToLive(100, TimeUnit.SECONDS)
+                .disableAutomaticRetries()
+                .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE).setDefaultHeaders(Collections.singletonList(
+                        new BasicHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36")
+                ))
+                .setDefaultConnectionConfig(ConnectionConfig.custom()
+                        .setBufferSize(2048)
+                        .build())
+                .setDefaultSocketConfig(SocketConfig.custom()
+                        .build())
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setMaxRedirects(3)
+                        .setCircularRedirectsAllowed(false)
+                        .setConnectionRequestTimeout(1000 * 3)
+                        .setSocketTimeout(1000 * 3)
+                        .setConnectionRequestTimeout(5 * 1000)
+                        .build());
+        client = builder.build();
+    }
+
+    @Override
+    public void handle(Request req) {
         try {
-            SslUtils.ignoreSsl();
-            builder.setConnectionManager(new PoolingHttpClientConnectionManager(RegistryBuilder.<ConnectionSocketFactory>create()
-                    .register("http", PlainConnectionSocketFactory.INSTANCE)
-                    .register("https", new SSLConnectionSocketFactory(SSLContext.getInstance("SSL")))
-                    .build()));
-        } catch (Exception e) {
-            logger.error("ignore ssl error", e);
-            System.exit(-1);
+            HttpGet get = new HttpGet(req.getUrl());
+            HttpResponse resp = client.execute(get);
+            Response response = new Response();
+            response.setData(EntityUtils.toByteArray(resp.getEntity()));
+            req.getCallback().handle(req, response);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-    client = builder.build();
-}
-
-@Override
-public void handle(Request req) {
-    try {
-        HttpGet get = new HttpGet(req.getUrl());
-        HttpResponse resp = client.execute(get);
-        Response response = new Response();
-        response.setData(EntityUtils.toByteArray(resp.getEntity()));
-        req.getCallback().handle(req, response);
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-}
 }
